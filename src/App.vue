1294715a309a8f51d839a8d98e35df6e39a34352
B-Musick <bmuze1@gmail.com>
2024-01-15 14:31:29 -0600

    Installed tailwind

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,11 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <nav>
+        <span>Brendan Musick</span>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <RouterLink to="/technologies">Technologies</RouterLink>
       </nav>
     </div>
   </header>
